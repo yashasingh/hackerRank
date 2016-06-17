22d31c3fc84898d647773330b16a233c732d1bbe
yasha singh <yashasingh7@gmail.com>
2016-06-18 00:03:22 +0530

    problem code - insertionsort2

---
+++ src/insertionsort.java
@@ -0,0 +1,51 @@
+/**
+ * Created by Yasha on 17-06-2016.
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class insertionsort {
+
+    public static void insertionSortPart2(int[] ar)
+    {
+
+        int n = ar.length;
+        for(int i=1; i<n;i++)
+        {
+            int key = ar[i];
+            int j=i-1;
+            while( j >=0 && ar[j]>key)
+            {
+                ar[j+1]= ar[j];
+                j=j-1;
+            }
+            ar[j+1]= key;
+            printArray(ar);
+        }
+        // Fill up the code for the required logic here
+        // Manipulate the array as required
+        // The code for Input/Output is already provided
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt();
+        }
+        insertionSortPart2(ar);
+
+    }
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+}
+
